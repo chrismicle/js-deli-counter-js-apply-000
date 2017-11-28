59556bce299605f263a44ef54b79b47a4beb7da3
Christina Collura <cmcollura@gmail.com>
2017-11-28 22:50:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, name){
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+function nowServing(katzDeli, name){
+  if(katzDeli === 0)
+}
